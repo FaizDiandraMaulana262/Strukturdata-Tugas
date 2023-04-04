93d8be8a14adc73243d13ec8a3e77d0a7ca2fe3e
BosToken <faizdiandra11@gmail.com>
2023-04-04 16:16:05 +0700

    latihanModul3

---
+++ modul3/latihan/MainQueue.java
@@ -0,0 +1,21 @@
+package modul3.latihan;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class MainQueue {
+    public void queueExample(){
+        Queue queue = new LinkedList();
+        queue.add("Java");
+        queue.add("DotNet");
+        queue.offer("PHP");
+        queue.offer("HTML");
+        System.out.println("Remove : "+queue.remove());
+        System.out.println("Element : "+queue.element());
+        System.out.println("Poll : "+queue.poll());
+        System.out.println("Peek : "+queue.peek());
+    }
+    public static void main(String[] args) {
+        new MainQueue().queueExample();
+    }
+}
